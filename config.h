bb016b36c38e7bc0b0d312748f39328a8b52a286
MGThePro <17652761+MGThePro@users.noreply.github.com>
2022-05-20 12:37:13 +0200

    Include array on config.h
    fixes compilation on good distros like arch

--- config.h
+++ config.h
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with yafetch; see the file COPYING.  If not see
 <http://www.gnu.org/licenses/>. */
 
+#include <array>
 #include <string>
 #include "yafetch.h"
 
